f6dbfd3e3c8992ca9594db2ce861f8c49ebbaab5
Berkay Gülyaz <berkay.gulyaz@emlakjet.com>
2023-01-04 00:05:41 +0300

    Customizing HTTP Requests

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=39,7505&lon=37,0150&appid=46470136edd4f53667ef9efa0cd415da';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=39,7505&lon=37,0150&appid=46470136edd4f53667ef9efa0cd415da&units=metric';
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+request({ url: url, json: true}, (error, response) => {
+    console.log(response)
+    console.log('It is currently ' + response.body.main.temp)
 })
