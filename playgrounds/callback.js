c2e0b3614f56626dec97057d1f337d4ab7d5d028
Berkay Gülyaz <berkay.gulyaz@emlakjet.com>
2023-01-05 01:14:18 +0300

    The Callback Function

---
+++ playgrounds/callback.js
@@ -0,0 +1,11 @@
+const geoCode = (address, callback) => {
+    const data = {
+        latitude: 0,
+        longtitude: 0,
+    }
+
+    return data
+}
+
+const data = geoCode('Philidelphia')
+console.log(data)
